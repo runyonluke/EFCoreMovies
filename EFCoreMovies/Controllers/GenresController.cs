810558e14dbf3482d0af4706cc3fc28b8de7c225
Luke Runyon <lrunyon@ara.com>
2024-01-19 10:08:25 -0600

    V 1.0.0 Swagger working, first endpoint working

---
+++ EFCoreMovies/Controllers/GenresController.cs
@@ -0,0 +1,22 @@
+﻿using EFCoreMovies.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EFCoreMovies.Controllers
+{
+    [ApiController]
+    [Route("api/genres")]
+    public class GenresController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        public GenresController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Genre> Get()
+        {
+            return context.Genres.ToList();
+        }
+    }
+}
