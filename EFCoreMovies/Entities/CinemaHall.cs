d73337169047d9b205a424e0be88fa78d30b2597
Luke Runyon <lrunyon@ara.com>
2024-01-25 08:47:52 -0600

    Foreign key attribute

--- EFCoreMovies/Entities/CinemaHall.cs
+++ EFCoreMovies/Entities/CinemaHall.cs
@@ -6,7 +6,9 @@
         public CinemaHallType CinemaHallType { get; set; }
         public decimal Cost { get; set; }
         public Currency Currency { get; set; }
-        public int CinemaId { get; set; }
+        public int? CinemaId { get; set; }
+        //if we wanted to use a key that isn't configured by convention
+        //[ForeignKey(nameof(NameOfKey))]
         public Cinema Cinema { get; set; }
         public HashSet<Movie> Movies { get; set; }
     }
